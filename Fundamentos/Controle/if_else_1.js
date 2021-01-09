4fa56baf7fd1d738d83821572161bcc8d465d9aa
LucasGullo <Lucas_Gullo@hotmail.com>
2021-01-09 13:54:48 -0300

    Mais sobre if/else

---
+++ Fundamentos/Controle/if_else_1.js
@@ -0,0 +1,16 @@
+const verdadeira = true;
+const falsa = false;
+
+
+if (verdadeira){
+    console.log("exec...");
+}
+else{
+    console.log("Não exec...");
+}
+if (falsa){
+    console.log("Não exec...");
+}
+else { 
+    console.log("exec...");
+}
